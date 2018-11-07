54123094fb62ec8f668e32a53c0f40f305b4be0c
Desire Hendricks <desire.hendricks@gmail.com>
2018-11-07 14:22:33 -0600

    Can't get localhost to work.

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from flask import Flask, request, redirect, render_template
-import cgi
-import os
-
 app = Flask(__name__)
-app.config['DEBUG'] = True
+#app.config['DEBUG'] = True
 
 # THIS CREATES ROUTE TO DISPLAY THE FORM
 
@@ -184,12 +181,12 @@ def user_signup_complete():
         return render_template('index.html', username_error=username_error, username=username, password_error=password_error, password=password, password_validate_error=password_validate_error, password_validate=password_validate, email_error=email_error, email=email)
 # THIS REDIRECTS TO A WELCOME PAGE
 
-@app.route('/welcome')
+""" @app.route('/welcome')
 def valid_signup():
     username = request.args.get('username')
     #template = jinja_env.get_template('welcome.html')
     
-    return render_template('welcome.html', username=username) #template.render(username=username)
+    return render_template('welcome.html', username=username) #template.render(username=username) """
 
 app.run()
 
